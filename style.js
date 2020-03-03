eae42a584c911e17649e535fe3e6eb70bb49d656
shiny <shiny.gao@foxmail.com>
2020-03-03 16:25:32 +0800

    DOM事件（循环圆形变换）
    Signed-off-by: shiny <shiny.gao@foxmail.com>

--- style.js
+++ style.js
@@ -1,8 +1,19 @@
-var n = 0
-$('div').on('click', function (e) {
-    setTimeout(() => {
-        $(e.currentTarget).addClass('active')
-    }, n * 1000);
-    //e.stopPropagation()
-    n += 1
-})
+let n = 0
+let divs = $('div').get()
+for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', () => {
+        setTimeout(() => {
+            divs[i].classList.add('active')
+        }, n * 500)
+        n += 1
+    }, true)  //从爷爷到孙子
+}
+
+for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', () => {
+        setTimeout(() => {
+            divs[i].classList.remove('active')
+        }, n * 500)
+        n += 1
+    })
+}
